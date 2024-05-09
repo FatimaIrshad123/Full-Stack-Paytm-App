341e87360ba3e28e92541ed025af851d520f9a24
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-09 21:12:28 +0500

    Updated db

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import db from '@repo/db/client'
 const app = express()
 
 app.post("/hdfcWebhook", (req, res) => {
-    //TDO: Add zod validation here?
+    //TODO: Add zod validation here?
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
+    
     // Update balance in db, add txn
 })
